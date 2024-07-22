26b2d8304c61f8ad9886a3ed227dc2dbcac50761
Tushar More <65149782+Tushar443@users.noreply.github.com>
2024-07-22 15:29:13 +0530

    Rabbit MQ Added

--- CompanyMicroservice/src/main/java/com/project/CompanyMicroservice/messaging/RabbitMQConfig.java
+++ CompanyMicroservice/src/main/java/com/project/CompanyMicroservice/messaging/RabbitMQConfig.java
@@ -5,14 +5,20 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+
     @Bean
-    public Queue companyReviewQueue(){
-        return new Queue("companyRatingQueue");
+    public Queue getQueue(){
+        return new Queue(queueName);
     }
 
     @Bean
--- CompanyMicroservice/src/main/java/com/project/CompanyMicroservice/messaging/ReviewMessageConsumer.java
+++ CompanyMicroservice/src/main/java/com/project/CompanyMicroservice/messaging/ReviewMessageConsumer.java
@@ -3,10 +3,15 @@ package com.project.CompanyMicroservice.messaging;
 import com.project.CompanyMicroservice.dto.RabbitMQ.ReviewMessage;
 import com.project.CompanyMicroservice.service.CompanyServiceImpl;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ReviewMessageConsumer {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
     private final CompanyServiceImpl companyService;
 
 
@@ -14,7 +19,7 @@ public class ReviewMessageConsumer {
         this.companyService = companyService;
     }
 
-    @RabbitListener(queues = "companyRatingQueue")
+    @RabbitListener(queues ="${rabbitmq.queue.name}")
     public void consumeMessage(ReviewMessage reviewMessage){
         companyService.updateCompanyRating(reviewMessage);
     }
