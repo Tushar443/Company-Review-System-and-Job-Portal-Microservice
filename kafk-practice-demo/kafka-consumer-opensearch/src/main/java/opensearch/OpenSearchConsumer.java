7a996d6a92218b662d7b84b21a9b40dc82ddb4e2
moret <moretushart33@gmail.com>
2025-03-17 21:41:25 +0530

    OpenSearch consumer created

--- kafk-practice-demo/kafka-consumer-opensearch/src/main/java/opensearch/OpenSearchConsumer.java
+++ kafk-practice-demo/kafka-consumer-opensearch/src/main/java/opensearch/OpenSearchConsumer.java
@@ -1,7 +1,130 @@
 package opensearch;
 
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.opensearch.action.index.IndexRequest;
+import org.opensearch.action.index.IndexResponse;
+import org.opensearch.client.RequestOptions;
+import org.opensearch.client.RestClient;
+import org.opensearch.client.RestHighLevelClient;
+import org.opensearch.client.indices.CreateIndexRequest;
+import org.opensearch.client.indices.GetIndexRequest;
+import org.opensearch.common.xcontent.XContentType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class OpenSearchConsumer {
-    public static void main(String[] args) {
-        System.out.println("Hello Consumer");
+    private static final Logger log = LoggerFactory.getLogger(OpenSearchConsumer.class);
+
+    public static void main(String[] args) throws IOException {
+        Logger logger = LoggerFactory.getLogger(OpenSearchConsumer.class.getSimpleName());
+        logger.info("Hello Consumer");
+        // first create OpenSearch Client
+        RestHighLevelClient openSearchClient = createOpenSearchClient();
+
+        // create our Kafka Client
+        KafkaConsumer<String,String> consumer = createKafkaConsumer();
+
+
+        // we need to create the index on OpenSearch i it doesn't exist already
+        try(openSearchClient;consumer){
+            boolean indexExist = openSearchClient.indices().exists(new GetIndexRequest("wikimedia"),RequestOptions.DEFAULT);
+            if(!indexExist){
+                CreateIndexRequest createIndexRequest = new CreateIndexRequest("wikimedia");
+                openSearchClient.indices().create(createIndexRequest, RequestOptions.DEFAULT);
+                logger.info("The Wikimedia Index has been created");
+            }else {
+                logger.info("The Wikimedia Index already exits");
+            }
+            // subscribe the consumer
+            consumer.subscribe(Collections.singleton("wikimedia.recentchange"));
+            while(true){
+                ConsumerRecords<String,String> records = consumer.poll(Duration.ofMillis(3000));
+
+                int recordCount = records.count();
+                logger.info("Recevied "+ recordCount + " record(s)");
+                for (ConsumerRecord<String,String> record : records){
+                    try{
+                        //send the record into OpenSearch
+                        IndexRequest indexRequest = new IndexRequest("wikimedia")
+                                .source(record.value(), XContentType.JSON);
+                        IndexResponse response = openSearchClient.index(indexRequest, RequestOptions.DEFAULT);
+                        logger.info("Response Id = "+response.getId());
+                    } catch (Exception e) {
+
+                    }
+                }
+            }
+        }
+
+
+        // main code logic
+
+        // close things
+    }
+
+    private static KafkaConsumer<String, String> createKafkaConsumer() {
+        String groupId = "consumer-opensearch-demo";
+
+        //create producer properties
+        Properties properties = new Properties();
+        //connect to localhost
+        properties.setProperty("bootstrap.servers","127.0.0.1:9092");
+
+        //set producer properties
+        properties.setProperty("key.deserializer", StringDeserializer.class.getName());
+        properties.setProperty("value.deserializer",StringDeserializer.class.getName());
+        properties.setProperty("group.id",groupId);
+        properties.setProperty("auto.offset.reset","latest");
+
+        //create consumer
+        return new KafkaConsumer<>(properties);
+    }
+
+    public static RestHighLevelClient createOpenSearchClient() {
+        String connString = "http://localhost:9200";
+//        String connString = "https://c9p5mwld41:45zeygn9hy@kafka-course-2322630105.eu-west-1.bonsaisearch.net:443";
+
+        // we build a URI from the connection string
+        RestHighLevelClient restHighLevelClient;
+        URI connUri = URI.create(connString);
+        // extract login information if it exists
+        String userInfo = connUri.getUserInfo();
+
+        if (userInfo == null) {
+            // REST client without security
+            restHighLevelClient = new RestHighLevelClient(RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), "http")));
+
+        } else {
+            // REST client with security
+            String[] auth = userInfo.split(":");
+
+            CredentialsProvider cp = new BasicCredentialsProvider();
+            cp.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(auth[0], auth[1]));
+
+            restHighLevelClient = new RestHighLevelClient(
+                    RestClient.builder(new HttpHost(connUri.getHost(), connUri.getPort(), connUri.getScheme()))
+                            .setHttpClientConfigCallback(
+                                    httpAsyncClientBuilder -> httpAsyncClientBuilder.setDefaultCredentialsProvider(cp)
+                                            .setKeepAliveStrategy(new DefaultConnectionKeepAliveStrategy())));
+
+
+        }
+
+        return restHighLevelClient;
     }
 }
